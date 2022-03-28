c9b5ec00d22deda59d3202620bc5c8113de3013f
chigov2 <chigov@gmail.com>
2022-03-28 16:39:30 +0300

    22

--- app/src/main/java/techmarket/uno/drawermenu002/MainActivity.java
+++ app/src/main/java/techmarket/uno/drawermenu002/MainActivity.java
@@ -1,14 +1,20 @@
 package techmarket.uno.drawermenu002;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawerLayout);
+       //toggle = new ActionBarDrawerToggle(this, drawerLayout,);
     }
 }
